f68c94d4db9b2095b3bde611b01adedc8793e864
Jens-k-m-m <jensmonteiro@gmail.com>
2022-05-30 16:36:32 +0200

    Stild work on  Function createRider

--- js/create.js
+++ js/create.js
@@ -1,3 +1,6 @@
+const out = function (str) {
+  console.log(str);
+}
 const riderId = document.getElementById('riderId');
 const riderFirstName = document.getElementById('riderFirstName');
 const riderLastName = document.getElementById('riderLastName');
@@ -10,19 +13,37 @@ const riderCountry = document.getElementById('riderCountry');
 async function createRider() {
   const url = "http://localhost:8080/create/riders";
   const riderData = {
-    riderId:riderId,
-    riderFirstName:riderFirstName,
-    riderLastName:riderLastName,
-    riderAge:riderAge,
-    riderTime:riderTime,
-    riderPoint:riderPoint,
+    riderId: riderId,
+    riderFirstName: riderFirstName,
+    riderLastName: riderLastName,
+    riderAge: riderAge,
+    riderTime: riderTime,
+    riderPoint: riderPoint,
     riderMountainPoints,
-    riderCountry:riderCountry
+    riderCountry: riderCountry,
     // Nest Objet in en objet finder specifik ind i objet, havd jeg skal bruge.
-    team:{
+    team: {
       teamId: teamId.value,
       teamName: teamName.value,
       teamCountry: teamCountry
     }
   }
+  out(riderData);
+  const response = await fetch(url, {
+    method: "POST",
+    body: JSON.stringify(data),
+    headers: {"Content-type": "application/json; charset=UTF-8"}
+  })
+  if (!response.ok) {
+    alert("Could not create");
+  } else {
+    alert("is created");
+  }
+  reloadTable();
 }
+
+create.addEventListener("click", () => {
+  console.log(riderId.value + "value");
+  createRider();
+})
+
