15e560e897b410aae72d97adb2277bd3dbd25739
Jens-k-m-m <jensmonteiro@gmail.com>
2022-05-30 19:22:07 +0200

    index

--- js/create.js
+++ js/create.js
@@ -1,7 +1,8 @@
 const out = function (str) {
   console.log(str);
 }
-const riderId = document.getElementById('riderId');
+
+// const riderId = document.getElementById('riderId');
 const riderFirstName = document.getElementById('riderFirstName');
 const riderLastName = document.getElementById('riderLastName');
 const riderAge = document.getElementById('riderAge');
@@ -9,35 +10,40 @@ const riderTime = document.getElementById('riderTime');
 const riderPoint = document.getElementById('riderPoint');
 const riderMountainPoints = document.getElementById('riderMountainPoints');
 const riderCountry = document.getElementById('riderCountry');
+const teamId = document.getElementById('select');
+const create = document.getElementById('create');
 
 async function createRider() {
   const url = "http://localhost:8080/create/riders";
+  out(url);
   const riderData = {
-    riderId: riderId,
     riderFirstName: riderFirstName,
     riderLastName: riderLastName,
     riderAge: riderAge,
     riderTime: riderTime,
     riderPoint: riderPoint,
-    riderMountainPoints,
+    riderMountainPoints:riderMountainPoints,
     riderCountry: riderCountry,
     // Nest Objet in en objet finder specifik ind i objet, havd jeg skal bruge.
     team: {
       teamId: teamId.value,
-      teamName: teamName.value,
-      teamCountry: teamCountry
+      teamName: teamId.value,
+      teamCountry: teamId.value
+
     }
+
   }
+
   out(riderData);
   const response = await fetch(url, {
     method: "POST",
-    body: JSON.stringify(data),
+    body: JSON.stringify(riderData),
     headers: {"Content-type": "application/json; charset=UTF-8"}
   })
   if (!response.ok) {
     alert("Could not create");
   } else {
-    alert("is created");
+    alert("is created" + riderData.riderFirstName);
   }
   reloadTable();
 }
---
+++ js/show.js
@@ -0,0 +1,92 @@
+const reset = document.getElementById('reset');
+reset.addEventListener("click", resetWeb);
+const table = document.getElementById('table');
+
+// find
+const input = document.getElementById('search-input');
+const search = document.getElementById('search');
+
+//find
+async function getRidersBySearch(searchFirstName) {
+  const url = "http://localhost:8080/findRiderByFirstName/riders/" + searchFirstName;
+  const resp = await fetch(url);
+  const respData = await resp.json();
+  console.log(respData);
+  resetTable();
+  addDataToRows(respData);
+}
+//find
+search.addEventListener("click", () => {
+  const firstName = input.value;
+
+  if (firstName) {
+    console.log(firstName+"frssss");
+
+    getRidersBySearch(firstName);
+  }
+
+})
+//find
+
+function resetTable() {
+  table.innerHTML="";
+  tableHeadlines();
+}
+function resetWeb() {
+  table.innerHTML="";
+  getCandidates()
+  tableHeadlines()
+}
+
+
+async function getCandidates() {
+  const url = "http://localhost:8080/show/riders";
+  const resp = await fetch(url);
+  const respData = await resp.json();
+  addDataToRows(respData);
+  console.log(respData + "respData WWAAZZA");
+}
+
+function tableHeadlines() {
+  let rowCount = table.rows.length;
+  let row = table.insertRow(rowCount);
+  row.insertCell(0).innerText = "Rider Id";
+  row.insertCell(1).innerText = "Rider First Name";
+  row.insertCell(2).innerText = "Rider Last Name";
+  row.insertCell(3).innerText = "Rider Age";
+  row.insertCell(4).innerText = "Rider Time";
+  row.insertCell(5).innerText = "Rider Point";
+  row.insertCell(6).innerText = "Rider Mountain Points";
+  row.insertCell(7).innerText = "Rider Country";
+  row.insertCell(8).innerHTML = "Team Id";
+  row.insertCell(9).innerHTML = "Team Name";
+  row.insertCell(10).innerHTML = "Team Country";
+
+}
+
+function addDataToRows(data) {
+  // const table = document.getElementById('storeList').getElementsByTagName('tbody')[0];
+  console.log(data + "dataWW")
+  data.forEach(element => {
+    let rowCount = table.rows.length;
+    let row = table.insertRow(rowCount);
+    row.insertCell(0).innerHTML = element.riderId;
+    row.insertCell(1).innerHTML = element.riderFirstName;
+    row.insertCell(2).innerHTML = element.riderLastName;
+    row.insertCell(3).innerHTML = element.riderAge;
+    row.insertCell(4).innerHTML = element.riderTime;
+    row.insertCell(5).innerHTML = element.riderPoint;
+    row.insertCell(6).innerHTML = element.riderMountainPoints;
+    row.insertCell(7).innerHTML = element.riderCountry;
+    row.insertCell(8).innerHTML = element.teamId;
+    row.insertCell(9).innerHTML = element.teamName;
+    row.insertCell(10).innerHTML = element.teamCountry;
+  })
+}
+function reloadTable(){
+
+  resetTable();
+  getCandidates();
+}
+reloadTable();
+
